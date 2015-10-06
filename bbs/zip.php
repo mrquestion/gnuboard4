e79aaa42dc7960efc3220cbecbd759b00514553d
R <reizen4u@gmail.com>
2015-10-06 10:02:04 +0900

    4.37.20

--- bbs/zip.php
+++ bbs/zip.php
@@ -5,10 +5,11 @@ $g4[title] = "도로명/지번 주소 검색";
 include_once("$g4[path]/head.sub.php");
 
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on') {   //https 통신
-    $g4['daum_juso_js'] = "<script src=\"https://spi.maps.daum.net/imap/map_js_init/postcode.js\"></script>";
-} else {  //http 통신 
-    $g4['daum_juso_js'] = "<script src=\"http://dmaps.daum.net/map_js_init/postcode.js\"></script>";
+    echo '<script src="https://spi.maps.daum.net/imap/map_js_init/postcode.js"></script>'.PHP_EOL;
+} else {  //http 통신
+    echo '<script src="http://dmaps.daum.net/map_js_init/postcode.js"></script>'.PHP_EOL;
 }
+echo '<script src="'.$g4['path'].'/js/zip.js"></script>'.PHP_EOL;
 
 $member_skin_path = "$g4[path]/skin/member/$config[cf_member_skin]";
 include_once("$member_skin_path/zip.skin.php");
--- skin/member/basic/zip.skin.php
+++ skin/member/basic/zip.skin.php
@@ -1,7 +1,5 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
-
-echo $g4['daum_juso_js'].PHP_EOL;
 ?>
 <style>
 #daum_juso_wrap{position:absolute;left:0;top:0;width:100%;height:100%}
@@ -10,40 +8,22 @@ echo $g4['daum_juso_js'].PHP_EOL;
 <div id="daum_juso_wrap" class="daum_juso_wrap"></div>
 
 <script>
-jQuery(function($){
-    $("html, body").addClass("daum_juso_body");
+function put_data2(zip1, zip2, addr1, addr2, addr3, jibeon)
+{
+    var of = window.opener.document.<?php echo $frm_name; ?>;
 
-    function put_data2(zip1, zip2, addr1, addr2, addr3, jibeon)
-    {
-        var of = window.opener.document.<?php echo $frm_name; ?>;
+    of.<?php echo $frm_zip1; ?>.value = zip1;
+    of.<?php echo $frm_zip2; ?>.value = zip2;
+    of.<?php echo $frm_addr1; ?>.value = addr1;
+    of.<?php echo $frm_addr2; ?>.value = addr2;
+    of.<?php echo $frm_addr3; ?>.value = addr3;
 
-        of.<?php echo $frm_zip1; ?>.value = zip1;
-        of.<?php echo $frm_zip2; ?>.value = zip2;
-        of.<?php echo $frm_addr1; ?>.value = addr1;
-        of.<?php echo $frm_addr2; ?>.value = addr2;
-        of.<?php echo $frm_addr3; ?>.value = addr3;
-
-        if( jibeon ){
-            if(of.<?php echo $frm_jibeon; ?> !== undefined){
-                of.<?php echo $frm_jibeon; ?>.value = jibeon;
-            }
+    if( jibeon ){
+        if(of.<?php echo $frm_jibeon; ?> !== undefined){
+            of.<?php echo $frm_jibeon; ?>.value = jibeon;
         }
-        of.<?php echo $frm_addr2; ?>.focus();
-        window.close();
     }
-
-    var el_id = document.getElementById('daum_juso_wrap');
-    new daum.Postcode({
-        oncomplete: function(data) {
-            var address1 = data.address1,
-                address2 = "";
-            if(data.addressType == "R"){        //도로명이면
-                address2 = data.address2;
-            }
-            put_data2(data.postcode1, data.postcode2, address1, '', address2, data.addressType);
-        },
-        width : '100%',
-        height : '100%'
-    }).embed(el_id);
-});
+    of.<?php echo $frm_addr2; ?>.focus();
+    window.close();
+}
 </script>
